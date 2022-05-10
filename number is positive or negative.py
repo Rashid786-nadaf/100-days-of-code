a1c755de0edd1520cfc4c2bf4386deea808102a2
Rashid786-nadaf <rashidnadaf85@gmail.com>
2022-05-10 12:09:43 +0530

    100_days_of_code

---
+++ number is positive or negative.py
@@ -0,0 +1,8 @@
+print("To check to wheather the number is positive or negative")
+num = int(input("Enter the value: "))
+if num == 0:
+    print("Number is zero")
+elif num > 0:
+    print("Number is positive")
+else:
+    print("Number is negative")
